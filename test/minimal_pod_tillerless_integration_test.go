1d769a3d23005cb3af703063ca0994e685b00d32
jay <richellin7@gmail.com>
2020-01-24 14:34:40 +0900

    fix: add packages to run tests independently

--- test/minimal_pod_tillerless_integration_test.go
+++ test/minimal_pod_tillerless_integration_test.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/gruntwork-io/terratest/modules/helm"
+	http_helper "github.com/gruntwork-io/terratest/modules/http-helper"
 	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/random"
 )
@@ -41,4 +43,31 @@ func TestPodDeploysContainerImageTillerless(t *testing.T) {
 	// Now that the chart is deployed, verify the deployment. This function will open a tunnel to the Pod and hit the
 	// nginx container endpoint.
 	verifyNginxPod(t, kubectlOptions, podName)
+}
+
+// verifyNginxPod will open a tunnel to the Pod and hit the endpoint to verify the nginx welcome page is shown.
+func verifyNginxPod(t *testing.T, kubectlOptions *k8s.KubectlOptions, podName string) {
+	// Wait for the pod to come up. It takes some time for the Pod to start, so retry a few times.
+	retries := 15
+	sleep := 5 * time.Second
+	k8s.WaitUntilPodAvailable(t, kubectlOptions, podName, retries, sleep)
+
+	// We will first open a tunnel to the pod, making sure to close it at the end of the test.
+	tunnel := k8s.NewTunnel(kubectlOptions, k8s.ResourceTypePod, podName, 0, 80)
+	defer tunnel.Close()
+	tunnel.ForwardPort(t)
+
+	// ... and now that we have the tunnel, we will verify that we get back a 200 OK with the nginx welcome page.
+	// It takes some time for the Pod to start, so retry a few times.
+	endpoint := fmt.Sprintf("http://%s", tunnel.Endpoint())
+	http_helper.HttpGetWithRetryWithCustomValidation(
+		t,
+		endpoint,
+		nil,
+		retries,
+		sleep,
+		func(statusCode int, body string) bool {
+			return statusCode == 200 && strings.Contains(body, "Welcome to nginx")
+		},
+	)
 }
